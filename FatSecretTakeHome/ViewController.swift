e3ba5c034f7fcd6d23c90039bc5b07e49e2f4df7
carminchameleon <hwanggee1211@gmail.com>
2023-10-17 21:12:58 +1100

    Initial Commit

---
+++ FatSecretTakeHome/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FatSecretTakeHome
+//
+//  Created by Eunji Hwang on 17/10/2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
